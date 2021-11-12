347dfe1a17aa2dafb690fcbe9d0db3f656a325cd
SShipon <shamsulislamshipon2022@gmail.com>
2021-11-13 00:05:29 +0200

    hey

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,106 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      <div className="pt-5 pb-2 bg-dark text-white container">
-        <div className="row gx-5">
-          <div className="col-lg-4">
-            <div className="bg-dark text-white">
-              <h4>AUTO <span className="text-warning">DEAL</span></h4>
+     <div className="main-footer">
+            <div className="container text-content">
+                <div className="row">
+                    {/*colum 1*/}
+                   <div className="col-md-3 col-sm-6">
+                        <h4>SEMRUSH</h4>
+                    <ul className="list-unstyled">
+                        <li>Designer</li>
+                        <li>Interactions</li>
+                        <li>CMS</li>
+                        <li>Ecommerce</li>
+                        <li>Accessibility</li>
+                        <li>Templates</li>
+                        <li>learning</li>
+                    </ul>
+                    </div>
+                      {/*colum 2*/}
+                   <div className="col-md-3 col-sm-6">
+                         <h4>COMPANY</h4>
+                    <ul className="list-unstyled">
+                        <li>Careers</li>
+                        <li>Accessibility Statement</li>
+                        <li>Merch Store</li>
+                        <li>Terms of Service</li>
+                        <li>Privacy Policy</li>
+                        <li>Cookie Preferences</li>
+                        <li>Become an Affiliate</li>
+                    </ul>
+                    </div>
+                      {/*colum 3*/}
+                   <div className="col-md-3 col-sm-6">
+                         <h4>HELP</h4>
+                    <ul className="list-unstyled">
+                        <li>About Colorlib</li>
+                        <li>Advertise </li>
+                        <li>WordPress Themes</li>
+                        <li>Website Templates</li>
+                        <li>Bootstrap Templates</li>
+                        <li>Support Forum</li>
+                        <li>Contact us</li>
+                    </ul>
+
+                    </div>
+                      {/*colum 4*/}
+                   <div className="col-md-3 col-sm-6">
+                         <h4>FOLLOW</h4>
+                    <ul className="list-unstyled font-awsome ">
+                        <li><i class="fas fa-home"></i> Dhaka dhanmondi</li>
+                        <li><i class="fas fa-home"></i> Comiila Kandirpar </li>
+                        <li><i class="fas fa-phone"></i> +8801703059461</li>
+                        <li><i class="fas fa-question-circle"></i> +4574754</li>
+                        <li><a href="https://www.facebook.com/mdshipon.chowdhury.940"><i class="fab fa-facebook"></i></a> Facebook</li>
+                        <li><a href="https://www.linkedin.com/in/md-shipon-214207214/"><i class="fab fa-linkedin-in"></i></a> Linkdin</li>
+                    </ul>
+                    </div>
+                </div>
+                {/*Footer Bottom */}
+                <div className="footer-bottom">
+
+                    <p className="copyright">&copy;{ new Date().getFullYear()} Webflow, Inc. All rights reserved shamsul islam shipon.</p>
+
+                </div>
             </div>
-            <div className="my-2">
-              <p>
-                <small> Hossain villa, Kuril, Dhaka</small>
-              </p>
-              <p>
-                <small> Official: 21hujaifa@gmail.com</small>
-              </p>
-              <p>
-                <small> Helpline: 01791158039</small>
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-2">
-            <div>
-              <p>Quick Links</p>
-              <ul className="p-0 bg-dark text-start">
-                <li className="list-group-item bg-dark p-0">
-                  <Link className="text-decoration-none text-white" to="/home">
-                    Home
-                  </Link>
-                </li>
-                <li className="list-group-item bg-dark p-0">
-                  <Link
-                    className="text-decoration-none text-white"
-                    to="/AddDestination"
-                  >
-                    Add Service
-                  </Link>
-                </li>
-                <li className="list-group-item bg-dark p-0">
-                  <Link className="text-decoration-none text-white" to="/manage">
-                    Manage
-                  </Link>
-                </li>
-                <li className="list-group-item bg-dark  p-0">
-                  <Link
-                    className="text-decoration-none text-white"
-                    to="/cart"
-                  >
-                    Cart
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-lg-2">
-            <div>
-              <p>Quick Links</p>
-              <ul className="p-0 bg-dark text-start">
-                <li className="list-group-item bg-dark p-0">
-                  <Link className="text-decoration-none text-white" to="/home">
-                    Home
-                  </Link>
-                </li>
-                <li className="list-group-item bg-dark p-0">
-                  <Link
-                    className="text-decoration-none text-white"
-                    to="/AddDestination"
-                  >
-                    Add Service
-                  </Link>
-                </li>
-                <li className="list-group-item bg-dark p-0">
-                  <Link className="text-decoration-none text-white" to="/manage">
-                    Manage
-                  </Link>
-                </li>
-                <li className="list-group-item bg-dark  p-0">
-                  <Link
-                    className="text-decoration-none text-white"
-                    to="/cart"
-                  >
-                    Cart
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="text-center">
-              <p>You can pay with</p>
-              <img className="img-fluid h-100"
-                src="https://www.sslwireless.com/wp-content/uploads/2020/05/Payment-gateway-service.jpg"
-                alt=""
-              />
-            </div>
-          </div>
         </div>
-      <p className="text-white text-center mt-3">Copyright © 2021 Traveller</p>
-      </div>
-    </div>
   );
 };
 
